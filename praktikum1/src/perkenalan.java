54a614bc406dadaf5ad32f9d3cee1bcb8307eaf4
Muhamad Fauzi <114374732+MuhFauziIT@users.noreply.github.com>
2023-10-05 17:54:08 +0700

    Praktikum 1
    Hello World menggunakan Java

---
+++ praktikum1/src/perkenalan.java
@@ -0,0 +1,6 @@
+public class perkenalan {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Saya Muhamad Fauzi");
+        System.out.println("Mahasiswa STT-NF 2022");
+    }
+}
